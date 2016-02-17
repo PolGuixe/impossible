01810d86b358b6593bf247a21501d3322fc46c7f
PolGuixe <pol.guixe@gmail.com>
2016-02-17 18:28:37 +0100

    created route for core module

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import {mount} from 'react-mounter';
 
-//import MainLayout from './components/main_layout.jsx';
-
+import MainLayout from './components/main_layout.jsx';
+import ItemList from '../items/containers/ItemList.jsx';
 
 export default function (injectDeps, {FlowRouter}) {
-  //const MainLayoutCtx = injectDeps(MainLayout);
+  const MainLayoutCtx = injectDeps(MainLayout);
 
-  // FlowRouter.route('/', {
-  //   name: 'posts.list',
-  //   action() {
-  //     mount(MainLayoutCtx, {
-  //       content: () => (<PostList />)
-  //     });
-  //   }
-  // });
+  FlowRouter.route('/', {
+    name: 'items.list',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => ()
+      });
+    }
+  });
 }
