f7908ff024c58293af0e9316893200ea4410f22a
PolGuixe <pol.guixe@gmail.com>
2016-02-29 23:42:48 +0100

    fix: react components encapsulated
    react components needs to be encapsulated inside single elements

--- client/modules/items/components/item_list.jsx
+++ client/modules/items/components/item_list.jsx
@@ -4,15 +4,17 @@ import {Row, Col, Glyphicon} from 'react-bootstrap';
 
 const ItemList = ({content = () => null }) => (
   <Row>
-    <Col xs={12} sm={6}>
-      <ItemProgress />
-    </Col>
-  </Row>
-  <Row className="show-grid">
-    <Col xs={12}>
-      <a href="/edit"><Glyphicon glyph="plus"></Glyphicon> New Item</a>
-    </Col>
-    <Item />
+    <Row>
+      <Col xs={12} sm={6}>
+        <ItemProgress />
+      </Col>
+    </Row>
+    <Row className="show-grid">
+      <Col xs={12}>
+        <a href="/edit"><Glyphicon glyph="plus"></Glyphicon> New Item</a>
+      </Col>
+      <Item />
+    </Row>
   </Row>
 );
 
