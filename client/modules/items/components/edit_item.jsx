18a8764dbdb1a4b030c078634912380d34190b30
PolGuixe <pol.guixe@gmail.com>
2016-02-29 17:47:41 +0100

    added back button to edit item

--- client/modules/items/components/edit_item.jsx
+++ client/modules/items/components/edit_item.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Col, Panel, Input, ButtonInput} from 'react-bootstrap';
+import {Col, Panel, Input, ButtonInput, Glyphicon} from 'react-bootstrap';
 
 const EditItem = ({ content = () => null}) => (
    <Col xs={12} sm={6} smOffset={3}>
      <Panel>
+       <a href="/"><Glyphicon glyph="chevron-left"></Glyphicon> Back to Items</a>
        <h1>Edit Item</h1>
        <form>
          <Input type="text" placeholder="Name" />
