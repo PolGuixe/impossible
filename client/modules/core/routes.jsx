14e54084bec3bb47474c9a73067d74cc2e714310
PolGuixe <pol.guixe@gmail.com>
2016-02-29 18:50:51 +0100

    added new user form + packages

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -4,6 +4,7 @@ import {mount} from 'react-mounter';
 import MainLayout from './components/main_layout.jsx';
 import ItemList from '../items/components/item_list.jsx';
 import EditItem from '../items/components/edit_item.jsx';
+import NewUser from '../users/components/new_user.jsx';
 
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
@@ -16,11 +17,21 @@ export default function (injectDeps, {FlowRouter}) {
       });
     }
   });
+
   FlowRouter.route('/edit',{
     name:'items.edit',
     action(){
       mount(MainLayoutCtx,{
         content: () => (<EditItem />)
+      });
+    }
+  });
+
+  FlowRouter.route('/register',{
+    name: 'users.new',
+    action(){
+      mount(MainLayoutCtx, {
+        content: () => (<NewUser />)
       });
     }
   });
