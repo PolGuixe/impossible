3533fd8f120764fe917cad45bae2338a33aeb02f
PolGuixe <pol.guixe@gmail.com>
2016-02-29 13:28:19 +0100

    added edit item
    modified the routes to include the path

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -3,6 +3,7 @@ import {mount} from 'react-mounter';
 
 import MainLayout from './components/main_layout.jsx';
 import ItemList from '../items/components/item_list.jsx';
+import EditItem from '../items/components/edit_item.jsx';
 
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
@@ -12,6 +13,14 @@ export default function (injectDeps, {FlowRouter}) {
     action() {
       mount(MainLayoutCtx, {
         content: () => (<ItemList />)
+      });
+    }
+  });
+  FlowRouter.route('/edit',{
+    name:'items.edit',
+    action(){
+      mount(MainLayoutCtx,{
+        content: () => (<EditItem />)
       });
     }
   });
--- client/modules/items/components/edit_item.jsx
+++ client/modules/items/components/edit_item.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import {Col, Panel, Input, ButtonInput} from 'react-bootstrap';
 
-class editItem extends React.Component {
-  render() {
-    const { error } = this.props;
+const EditItem = ({ content = () => null}) => (
+   <Col xs={12} sm={6} smOffset={3}>
+     <Panel>
+       <h1>Edit Item</h1>
+       <form>
+         <Input type="text" placeholder="Name" />
+         <Input type="textarea" placeholder="Description" />
+         <ButtonInput bsStyle="primary" type="submit" value="Save Item" />
+       </form>
+     </Panel>
+   </Col>
+);
 
-    return (
-      <div>
-      </div>
-    );
-  }
-}
-
-export default editItem;
+export default EditItem;
