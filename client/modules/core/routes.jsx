b9eb80be665f42e955f9b8f1caf2b688741553ad
PolGuixe <pol.guixe@gmail.com>
2016-03-01 21:14:57 +0100

    added container for new_user

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -4,7 +4,7 @@ import {mount} from 'react-mounter';
 import MainLayout from './components/main_layout.jsx';
 import ItemList from '../items/components/item_list.jsx';
 import EditItem from '../items/components/edit_item.jsx';
-import NewUser from '../users/components/new_user.jsx';
+import NewUser from '../users/containers/new_user';
 
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
---
+++ client/modules/users/containers/new_user.js
@@ -0,0 +1,22 @@
+import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
+import NewUser from '../components/new_user.jsx';
+
+export const composer = ({context, clearErrors}, onData) => {
+  const {LocalState} = context();
+  const error = LocalState.get('CREATE_USER_ERROR');
+  onData(null, {error});
+
+  //clearErrors when unmounting the component
+  return clearErrors;
+};
+
+export const depsMapper = (context, actions) => ({
+  create: actions.users.create,
+  clearErrors: actions.users.clearErrors,
+  context: () => context
+});
+
+export default composeAll(
+  composeWithTracker(composer),
+  useDeps(depsMapper)
+)(NewUser);
